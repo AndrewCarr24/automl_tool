36a6adda53e0bbaa65b4c86c1354aa28da4ec5e4
Andrew Carr <carr.andrewjohn@gmail.com>
2024-12-13 20:32:46 -0500

    update

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
+# Import submodules to make them available at the package level
 from .automl import AutoML
+from .preprocessing import Prepreprocessor
+from .estimation import XGBWithEarlyStoppingClassifier, XGBWithEarlyStoppingRegressor
+from .plotting import PlotTools
